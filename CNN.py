9ddf9c8528bc71cee7b1b01bf5181e30ca67c84d
simran Singh <simransingh9503@gmail.com>
2018-09-17 16:55:09 +1200

    Put in For loop for multiple files

--- CNN.py
+++ CNN.py
@@ -3,33 +3,34 @@ from tensorflow.python.keras._impl.keras.models import Sequential
 from tensorflow.python.keras._impl.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import pickle
 
-X = pickle.load(open("X.pickle", "rb"))
-y = pickle.load(open('y.pickle', 'rb'))
+for file in range (50,100,50):
+    X = pickle.load(open("X"+str(file)+".pickle", "rb"))
+    y = pickle.load(open('y'+str(file)+'.pickle', 'rb'))
 
-X = X/255.0
+    X = X/255.0
 
-model = Sequential()
-model.add(Conv2D(128, (3, 3), input_shape = X.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+    model = Sequential()
+    model.add(Conv2D(128, (3, 3), input_shape = X.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(128, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(128, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(128, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(128, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=3, validation_split=0.1)
+    model.fit(X, y, batch_size=32, epochs=1, validation_split=0.1)
