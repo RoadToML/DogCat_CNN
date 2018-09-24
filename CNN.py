7117580e981079b8d6dc1837eb1368ebb7fcb203
simran Singh <simransingh9503@gmail.com>
2018-09-24 14:32:33 +1200

    multiple epoc

--- CNN.py
+++ CNN.py
@@ -4,13 +4,14 @@ from tensorflow.python.keras._impl.keras.layers import Dense, Dropout, Activatio
 from matplotlib import pyplot
 import pickle
 
-for file in range (200,250,50):
-    X = pickle.load(open("X"+str(file)+".pickle", "rb"))
-    y = pickle.load(open('y'+str(file)+'.pickle', 'rb'))
+for files in range (50,100,50):
+    X = pickle.load(open("X"+str(files)+".pickle", "rb"))
+    y = pickle.load(open('y'+str(files)+'.pickle', 'rb'))
 
     X = X/255.0
 
     model = Sequential()
+    
     model.add(Conv2D(128, (3, 3), input_shape = X.shape[1:]))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
@@ -34,14 +35,30 @@ for file in range (200,250,50):
                   optimizer='adam',
                   metrics=['accuracy', 'mae', 'mse'])
 
-    history = model.fit(X, y, batch_size=250, epochs=1, validation_split=0.1)
+    for epoch in range(1, 5, 1):
+        history = model.fit(X, y, batch_size=250, epochs=epoch, validation_split=0.1)
+        with open('output.txt', 'a') as f:
+            print(' ', file = f)
+            print('For File: ' + str(files), file=f)
+            print('Epochs: ' + str(epoch), file = f)
+            print('Hidden Layers: 3', file = f)
+            print('Mean Squared Error: '+str(history.history['mean_squared_error']), file=f)
+            print('Mean Absolute Error: '+str(history.history['mean_absolute_error']), file = f)
+            print('Accuracy: '+str(history.history['val_acc']), file = f)
 
-    pyplot.plot(history.history['mean_squared_error'])
-    pyplot.plot(history.history['mean_absolute_error'])
-    pyplot.plot(history.history['val_acc'])
-    pyplot.show()
+##    pyplot.plot(history.history['mean_squared_error'])
+##    pyplot.plot(history.history['mean_absolute_error'])
+##    pyplot.plot(history.history['val_acc'])
+##    pyplot.show()
 
+    
     print('****************************************************************' \
+          '****************************************************************' \
+          '****************************************************************' \
+          '****************************************************************' \
+          '****************************************************************' \
+          '****************************************************************' \
+          '****************************************************************' \
           '****************************************************************' \
           '****************************************************************' \
           '****************************************************************' \
