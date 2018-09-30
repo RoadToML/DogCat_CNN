6e32626886ab0f01199fd693ea941aa6bc107d5d
simran Singh <simransingh9503@gmail.com>
2018-09-30 16:11:27 +1300

    weights, color & momentum edit

--- CNN.py
+++ CNN.py
@@ -1,12 +1,16 @@
 import tensorflow as tf
+import numpy
 from tensorflow.python.keras._impl.keras.models import Sequential
 from tensorflow.python.keras._impl.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.python.keras._impl.keras import optimizers
 from matplotlib import pyplot
-import pickle
+import pickle        
 
-for files in range (50,100,50):
-    X = pickle.load(open("X"+str(files)+".pickle", "rb"))
-    y = pickle.load(open('y'+str(files)+'.pickle', 'rb'))
+numpy.set_printoptions(threshold=numpy.nan)
+
+for files in range (30,180,30):
+    X = pickle.load(open("X"+str(files)+"color.pickle", "rb"))
+    y = pickle.load(open('y'+str(files)+'color.pickle', 'rb'))
 
     X = X/255.0
 
@@ -20,54 +24,52 @@ for files in range (50,100,50):
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
-    model.add(Conv2D(128, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+##    model.add(Conv2D(128, (3, 3)))
+##    model.add(Activation('relu'))
+##    model.add(MaxPooling2D(pool_size=(2,2)))
 
     model.add(Flatten())
-    model.add(Dense(64))
-    model.add(Activation('relu'))
+##    model.add(Dense(64))
+##    model.add(Activation('relu'))
 
-    model.add(Dense(1))
+    model.add(Dense(64))#, activation = 'sigmoid'))
     model.add(Activation('sigmoid'))
 
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy', 'mae', 'mse'])
+    model.compile(loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy', 'mae', 'mse'], optimizer='sgd')
 
-    for epoch in range(1, 5, 1):
-        history = model.fit(X, y, batch_size=250, epochs=epoch, validation_split=0.1)
-        with open('output.txt', 'a') as f:
-            print(' ', file = f)
-            print('For File: ' + str(files), file=f)
-            print('Epochs: ' + str(epoch), file = f)
-            print('Hidden Layers: 3', file = f)
-            print('Mean Squared Error: '+str(history.history['mean_squared_error']), file=f)
-            print('Mean Absolute Error: '+str(history.history['mean_absolute_error']), file = f)
-            print('Accuracy: '+str(history.history['val_acc']), file = f)
+    for l_r in range (1, 2, 1):
+        l_r /= 8
+        
+        for mom in range (1, 2, 1):
+            mom /= 8
+            optimizers.SGD(lr = l_r, momentum = mom)
+
+            history = model.fit(X, y, batch_size=300, epochs=1, validation_split=0.1)
+            
+            weights = [layer.get_weights() for layer in model.layers]
+
+            with open('output.txt', 'a') as f, open('weights.txt', 'a') as f2:
+                print(' ', file = f)
+                print('For File: ' + str(files) + " color", file=f)
+                print(' ', file = f2)
+                print('For File: ' + str(files) + " color", file=f2)
+                print(weights, file = f2)
+                print('Epochs: 2', file = f)
+                print('Learning Rate: ' +str(l_r), file = f)
+                print('Momentum: ' +str(mom), file = f)
+                print('Hidden Layers: 2', file = f)
+                print('Mean Squared Error: '+str(history.history['mean_squared_error']), file=f)
+                print('Mean Absolute Error: '+str(history.history['mean_absolute_error']), file = f)
+                print('Accuracy: '+str(history.history['val_acc']), file = f)
+                
 
 ##    pyplot.plot(history.history['mean_squared_error'])
 ##    pyplot.plot(history.history['mean_absolute_error'])
 ##    pyplot.plot(history.history['val_acc'])
 ##    pyplot.show()
 
-    
-    print('****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************' \
-          '****************************************************************')
-          
+                      
 
 
     
